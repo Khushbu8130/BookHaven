974a75bc2e2914687efe1e418b90e6df0f342bed
KHUSHBU KUMARI <131510344+Khushbu8130@users.noreply.github.com>
2025-09-19 12:13:46 +0530

    Update FormElement.jsx

--- Frontend/src/components/contact/FormElement.jsx
+++ Frontend/src/components/contact/FormElement.jsx
@@ -1,9 +1,6 @@
 
-
-
-
 export default ({ type, label, placeholder, fieldRef, hasError }) => {
-    const classes = 'form-control w-full px-3 py-1.5 text-gray-700 rounded border border-solid border-gray-300 focus:border-pink-600 focus:outline-none';
+    const classes = 'form-control w-full px-3 py-1.5 text-white-700 rounded border border-solid border-gray-300 focus:border-pink-600 focus:outline-none';
     return (
         <div className="form-group mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2">
