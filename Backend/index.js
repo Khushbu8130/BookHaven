42feae511ca582059ecea213c92b9e72a7804a61
Khushbu Kumari <kumarikhushbu7199@gmail.com>
2025-11-01 21:04:17 +0530

    Add root route for render

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 import booklistRoute from "./route/booklist.route.js";
 
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
